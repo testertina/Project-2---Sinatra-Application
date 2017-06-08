371d3dd49ca566f7160b13bc84ab805d2759480e
tgohil04 <tgohil@spartaglobal.co>
2017-06-08 10:53:26 +0100

    Created basic SQL file

---
+++ seed.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS post;
+
+CREATE TABLE post (
+
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(200) NOT NULL,
+  cuisine VARCHAR(20), TEXT NOT NULL,
+  location TEXT NOT NULL,
+  cost 
+  opening_times
+  restaurant_id
+
+);
+
+INSERT INTO post (name , cuisine , location , cost , opening_times , restaurant_id) VALUES ('Post 1' , 'Body text 1');
