7d6e37425b5cc2f7f7cd8893fe5aecdece82c4bf
tgohil04 <tgohil@spartaglobal.co>
2017-06-08 12:27:07 +0100

    Created 7 RESTful routes in  restaurant and review controllers.

--- controllers/restaurants_controller.rb
+++ controllers/restaurants_controller.rb
@@ -1,6 +1,27 @@
 # Index
+get "/restaurants" do
+end
+
 # New
+get "/restaurants/new" do
+end
+
 # Create
+post "/restaurants" do
+end
+
 # Show
+get "/restaurants/:id" do
+end
+
+# Edit
+get "restaurants/:id" do
+end
+
 # Update
+put "/restaurants/:id" do
+end
+
 # Delete
+delete "/restaurants/:id" do
+end
---
+++ controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+# Index
+get "/reviews" do
+end
+
+# New
+get "/reviews/new" do
+end
+
+# Create
+post "/reviews" do
+end
+
+# Show
+get "/reviews/:id" do
+end
+
+# Edit
+get "reviews/:id" do
+end
+
+# Update
+put "/reviews/:id" do
+end
+
+# Delete
+delete "/restaurants/:id" do
+end
