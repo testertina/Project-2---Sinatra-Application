b07c884358942864a3d417019faa8b7a3f7f108a
tgohil04 <tgohil@spartaglobal.co>
2017-06-08 16:03:08 +0100

    Created review migration.

--- db/migrate/20170608134843_restaurants.rb
+++ db/migrate/20170608134843_restaurants.rb
@@ -1,10 +1,14 @@
-class Restaurants < ActiveRecord::Migration[5.1]
+class CreateRestaurants < ActiveRecord::Migration[5.1]
   def change
   	create_table :restaurants do |t|
+  		t.primary_key :restaurant_id
   		t.text :restaurant_name, VARCHAR(200), NOT NULL
   		t.text :cuisine_type, VARCHAR(20), NOT NULL
   		t.text :location, NOT NULL
   		t.text :price, VARCHAR(4) CHECK (cost == '£' OR cost == '££' OR cost == '£££' OR cost == '££££')
-  		t.text :opening_times NOT NULL
+  		t.text :opening_times, NOT NULL
+  		t.float :latitude, NOT NULL
+  		t.float :longitude, NOT NULL
+  	end
   end
 end
--- db/migrate/20170608135132_reviews.rb
+++ db/migrate/20170608135132_reviews.rb
@@ -1,4 +1,17 @@
-class Reviews < ActiveRecord::Migration[5.1]
+class CreateReviews < ActiveRecord::Migration[5.1]
   def change
+  	  create_table :reviews do |t|
+  	  	t.references :restaurant, index: true		
+	  		t.text :restaurant_name, VARCHAR(200), NOT NULL
+	  		t.text :cuisine_type, VARCHAR(20), NOT NULL
+	  		t.text :location, NOT NULL
+	  		t.text :price, VARCHAR(4) CHECK (cost == '£' OR cost == '££' OR cost == '£££' OR cost == '££££')
+	  		t.text :opening_times, NOT NULL
+	  		t.text :review_text, NOT NULL 
+
+	  		t.timestamps
+					
+	  	end
+	  	add_foreign_key :reviews, :restaurants, column: :restaurant_id	
   end
 end
