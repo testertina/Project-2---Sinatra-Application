2b3059755e34b7e04fa0519facfd99d820757f16
tgohil04 <tgohil@spartaglobal.co>
2017-06-08 16:46:30 +0100

    Edited the migrations to include appropiate validations.

--- db/migrate/20170608134843_restaurants.rb
+++ db/migrate/20170608134843_restaurants.rb
@@ -2,13 +2,13 @@ class CreateRestaurants < ActiveRecord::Migration[5.1]
   def change
   	create_table :restaurants do |t|
   		t.primary_key :restaurant_id
-  		t.text :restaurant_name, VARCHAR(200), NOT NULL
-  		t.text :cuisine_type, VARCHAR(20), NOT NULL
-  		t.text :location, NOT NULL
-  		t.text :price, VARCHAR(4) CHECK (cost == '£' OR cost == '££' OR cost == '£££' OR cost == '££££')
-  		t.text :opening_times, NOT NULL
-  		t.float :latitude, NOT NULL
-  		t.float :longitude, NOT NULL
+  		t.string :restaurant_name, :limit => 200 null: false
+  		t.string :cuisine_type, :limit => 20 null: false
+  		t.string :location null: false
+  		t.string :price, :limit => 4 :price => '£', :price => '££', :price => '£££', :price => '££££'
+  		t.string :opening_times, null: false
+  		t.float :latitude, null: false
+  		t.float :longitude, null: false
   	end
   end
 end
--- db/migrate/20170608135132_reviews.rb
+++ db/migrate/20170608135132_reviews.rb
@@ -2,12 +2,12 @@ class CreateReviews < ActiveRecord::Migration[5.1]
   def change
   	  create_table :reviews do |t|
   	  	t.references :restaurant, index: true		
-	  		t.text :restaurant_name, VARCHAR(200), NOT NULL
-	  		t.text :cuisine_type, VARCHAR(20), NOT NULL
-	  		t.text :location, NOT NULL
-	  		t.text :price, VARCHAR(4) CHECK (cost == '£' OR cost == '££' OR cost == '£££' OR cost == '££££')
-	  		t.text :opening_times, NOT NULL
-	  		t.text :review_text, NOT NULL 
+	  		t.string :restaurant_name, :limit => 200, null: false
+	  		t.string :cuisine_type, :limit => 20, null: false
+	  		t.string :location, null: false
+	  		t.string :price, :limit => 4 :price => '£', :price => '££', :price => '£££', :price => '££££'
+	  		t.string :opening_times, null: false
+	  		t.text :review_text, null: false 
 
 	  		t.timestamps
 					
