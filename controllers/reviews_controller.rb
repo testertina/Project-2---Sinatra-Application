ba28643994e99a8148747e75516e57950e870e32
tgohil04 <tgohil@spartaglobal.co>
2017-06-09 14:51:17 +0100

    Edited review controller so reviews now appear on restaurant show page.

--- controllers/reviews_controller.rb
+++ controllers/reviews_controller.rb
@@ -1,37 +1,30 @@
-# Index: user can view all reviews.
-# get "/reviews" do
-# 	@reviews = Review.all
-# 	erb :"reviews/index"
-# end
-
-# # New
-# get "/reviews/new" do
-# end
-
-# Create
+# Create: user can create a reviw for a restaurant.
 post "/restaurants/:id/reviews" do
 	@restaurant = Restaurant.find(params[:id]) # Finds a restaurant with specific id.
+	# Creates a review in the table reviews.
 	@review = Review.create(restaurant_id: @restaurant.id, restaurant_name: @restaurant.restaurant_name, cuisine_type: @restaurant.cuisine_type, location: @restaurant.location, price: @restaurant.price, opening_times: @restaurant.opening_times, review_text: params[:review_text])
  	redirect("/restaurants/#{@restaurant.id}/reviews")
 end
 
-# Index user can view a specific restaurant and their reviews.
+# Index: user can view a specific restaurant and their reviews.
 get "/restaurants/:id/reviews" do
 	@restaurant = Restaurant.find(params[:id]) # Finds a restaurant with specific id.
-	@reviews = Review.all
-	# @review = Review.find_by(restaurant_id: @restaurant.id) # Finds a review with same id as restaurant.
+	@reviews = Review.all # Lists all reviews on this resturant
+	@review = Review.find_by(restaurant_id: @restaurant.id) # Finds a review with same id as restaurant.
 	erb :"reviews/index"
 end
 
-
 # Edit
-get "reviews/:id" do
+get "/restaurants/:id/reviews/:id2" do
+	"hello world"
 end
 
 # Update
-put "/reviews/:id" do
+put "/restaurants/:id/reviews/:id2" do
+	"Update"
 end
 
 # Delete
-delete "/restaurants/:id" do
+delete "/restaurants/:id/reviews/:id2" do
+	"Delete"
 end
