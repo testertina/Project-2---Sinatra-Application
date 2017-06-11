e2c48886fd9c9bb070737247740f46a7216eba14
tgohil04 <tgohil@spartaglobal.co>
2017-06-12 00:00:45 +0100

    Created public folder and made CSS and JS files.

--- models/restaurant.rb
+++ models/restaurant.rb
@@ -6,4 +6,9 @@ class Restaurant < ActiveRecord::Base
 
   geocoded_by :post_code
   after_validation :geocode
+
+  configure :development do
+		set :public_folder, 'public'
+  	register Sinatra::Reloader
+	end
 end
--- models/review.rb
+++ models/review.rb
@@ -2,4 +2,9 @@ class Review < ActiveRecord::Base
 	
   has_one :restaurant
 
+  configure :development do
+		set :public_folder, 'public'
+  	register Sinatra::Reloader
+	end
+
 end
