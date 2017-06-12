8f4fb45ae43fcbd1fd24166b4686b375aad8c749
tgohil04 <tgohil@spartaglobal.co>
2017-06-12 19:24:47 +0100

    Fixed delete restaurant bug.  Included dependancy on reviews

--- models/restaurant.rb
+++ models/restaurant.rb
@@ -2,7 +2,7 @@ require "geocoder"
 
 class Restaurant < ActiveRecord::Base
 	extend Geocoder::Model::ActiveRecord
-  has_many :reviews  
+  has_many :reviews, dependent: :destroy  
 
   geocoded_by :post_code
   after_validation :geocode
